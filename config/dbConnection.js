0640fe2467fc068bae4e97068ecc11f1d5b87d73
Ahmeddaud11 <shahwah7788@gmail.com>
2025-11-30 00:17:49 +0300

    connected to db

---
+++ config/dbConnection.js
@@ -0,0 +1,24 @@
+import { connect } from "mongoose";
+
+
+const dbConnection = async () =>{
+    try{
+        const MONGO_URL = process.env.MONGO_URL;
+
+        if(!MONGO_URL){
+            console.log("cant find mongo url")
+
+        }
+
+        const MongoUrl = await connect(process.env.MONGO_URL,);
+        console.log(`database connected to ${MongoUrl.connection.host}`);
+
+    }catch(error){
+        console.log(error);
+        process.exit(1);
+    
+    }
+
+}
+
+export default dbConnection;
--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 import express, { urlencoded } from 'express'
 import dotenv from 'dotenv'
+import passport from 'passport'
+import dbConnection from './config/dbConnection.js'
 
 
 
-dotenv.config()
+
+dotenv.config();
+dbConnection();
 const app = express();
+
 
 //middleware
 app.use(express.json({limit: '100mb'}));
 app.use(urlencoded({limit: '100mb', extended: true}));
+app.use(passport.initialize())
 
 
 
