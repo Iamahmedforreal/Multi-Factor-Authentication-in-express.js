a0eba9d175ac7caad1696be716df80f04703e1fe
Ahmeddaud11 <shahwah7788@gmail.com>
2025-11-30 00:27:34 +0300

    i made userSchema for my database

---
+++ models/user.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.userSchema({
+    username:{
+        type: String,
+        unique: true,
+        required: true,
+    },
+    password:{
+        type: String,
+        unique: true,
+        required: true,
+    },
+    IsMfaActive:{
+        type: Boolean,
+        default: false,
+    },
+
+
+    TwoFactorSecrete:{
+        type: String,
+    },
+    
+    createdAt:{
+        type: Date,
+        default: Date.now,
+    },
+    updateAt:{
+        type: Date,
+        default: Date.now,
+    }
+
+})
+const User = mongoose.model('User', userSchema);
+
+export default User;
