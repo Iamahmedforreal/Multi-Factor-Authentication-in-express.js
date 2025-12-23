06ef81f0e7d2b0818f2a32ba6096e10a780e0dd0
Ahmeddaud11 <shahwah3344@gmail.com>
2025-12-23 20:20:16 +0300

    did rate limiter for endpoint

--- middleware/bruteForceLimiter.js
+++
@@ -1,9 +0,0 @@
-import rateLimit from "express-rate-limit";
-
-const loginLimiter = rateLimit({
-    windowMs:15 * 60 * 1000,
-    max: 5,
-    message:"too many login attempt try again later"
-});
-
-export default loginLimiter;
--- route/AuthRoutes.js
+++ route/AuthRoutes.js
@@ -1,7 +1,7 @@
 import express from "express";
 import passport from "passport";
 const router = express.Router();
-import loginLimiter from "../middleware/bruteForceLimiter.js";
+import globalratelimiter from "../middleware/ratelimiter.js";
 
 import { 
     login,
@@ -16,22 +16,29 @@ import {
     resetPassword,
     verifyLogin,
     verifySetup,
-
+    changePassword,
+    logoutAllSessions
 
 } from "../controller/authController.js";
 
-
+//pulic route
 router.post("/register",register);
-router.post("/login", loginLimiter ,passport.authenticate('local' , {session: false}), login);
 router.post("/logout", logout);
-router.get("/status", passport.authenticate('jwt', {session:false}), userStatus);
-router.post("/2fa/setup" , passport.authenticate('jwt', {session:false}), mfa);
-router.post("/2fa/login/verify" , passport.authenticate('temp-jwt', {session:false}), verifyLogin)
-router.post("/2fa/setup/verify" , passport.authenticate('jwt', {session:false}), verifySetup)
-router.post("/2fa/reset", passport.authenticate('jwt',{session:false}) ,resetmfa);
-router.get("/refresh" ,refresh);
 router.get("/verify-email" , verifyEmail);
 router.post("/forgot-password", forgotPassword);
-router.post("/reset-password" , resetPassword)
+router.post("/reset-password" , resetPassword);
+router.post("/login", globalratelimiter ,passport.authenticate('local' , {session: false}), login);
+
+//private route
+router.get("/status", passport.authenticate('jwt', {session:false}), userStatus);
+router.post("/2fa/setup" , passport.authenticate('jwt', {session:false}), mfa);
+router.post("/2fa/login/verify" ,globalratelimiter ,  passport.authenticate('temp-jwt', {session:false}), verifyLogin)
+router.post("/2fa/setup/verify" ,globalratelimiter ,  passport.authenticate('jwt', {session:false}), verifySetup)
+router.post("/2fa/reset", passport.authenticate('jwt',{session:false}) ,resetmfa);
+router.get("/refresh" , globalratelimiter, refresh);
+
+router.post("/change-password" , passport.authenticate('jwt', {session:false}), changePassword);
+router.post("/logout-all" , passport.authenticate('jwt', {session:false}), logoutAllSessions);
+
 
 export default router;
