0b7dd305015ee5072b3277ee6cabb2856729c178
Ahmeddaud11 <shahwah3344@gmail.com>
2026-02-01 02:35:37 +0300

    comment

--- utils/token.js
+++ utils/token.js
@@ -1,14 +1,14 @@
 
 import tokenService from '../services/tokenService.js';
-
+//generate tokens using tokenService
 export const generateAccessToken = (user) => {
     return tokenService.generateAccessToken(user);
 }
-
+//generate refresh token using tokenService
 export const generateRefreshToken = (user) => {
     return tokenService.generateRefreshToken(user);
 }
-
+//generate temporary token using tokenService
 export const genarateTemporaryToken = (user) => {
     return tokenService.generateTemporaryToken(user);
 }
