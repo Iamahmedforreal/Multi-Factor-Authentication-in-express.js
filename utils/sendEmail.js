901b4c23fede7f63bcf48c18ae62415a32d704f9
Ahmeddaud11 <shahwah3344@gmail.com>
2026-02-04 17:37:13 +0300

    comments on sendEmail

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,18 +1,18 @@
 
 import emailService from '../services/emailService.js';
-
+// Utility functions to send different types of emails
 export const sendEmailVerification = async (email, token) => {
     return await emailService.sendEmailVerification(email, token);
 }
-
+//send password reset email
 export const sendEmailResetPassword = async (email, token) => {
     return await emailService.sendPasswordResetEmail(email, token);
 }
-
+//send security warning email
 export const sendWarningEmail = async (email, action, meta = {}) => {
     return await emailService.sendSecurityWarning(email, action, meta);
 }
-
+//send new device login alert email
 export const sendLoginAlertEmail = async (email, action, meta = {}) => {
     return await emailService.sendNewDeviceLoginAlert(email, action, meta);
 }
